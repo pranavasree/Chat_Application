67f8050a41bf789cd7eeac0bb159f0be0ddda8f9
pranavasree <pranavareddy27@gmail.com>
2026-01-24 07:26:39 -0500

    UserModel Setup

---
+++ server/models/UserModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { genSalt } from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: [true, "Email is required"], unique: true },
+  password: { type: String, required: [true, "Password is required"] },
+  firstName: { type: String, required: false },
+  lastName: { type: String, required: false },
+  image: { type: String, required: false },
+  color: { type: Number, required: false },
+  profileSetup: { type: Boolean, default: false },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await genSalt();
+  this.password = await hash(this.password, salt);
+  next();
+});
+
+export const User = mongoose.model("User", userSchema);
